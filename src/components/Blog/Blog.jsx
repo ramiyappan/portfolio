7d592cc291e80a3370458c32a5d9934fc9871415
ramiyappan <ramiyappan98@gmail.com>
2024-06-22 10:42:36 -0400

    update blog.jsx

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -5,6 +5,8 @@ const Blog = () => {
     return (
         <section className="cta-section bg-light py-5">
             <div className="container text-center">
+                <h2 className="heading">A Blog about Software Development and Life</h2>
+                <div className="intro">Welcome to my blog. Subsribe and get my latest blog post in your inbox</div>
                 
             </div>
         </section>
