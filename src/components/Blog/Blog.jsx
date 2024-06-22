c0acf5e988103104595487f731721484bd97c70c
ramiyappan <ramiyappan98@gmail.com>
2024-06-22 10:49:43 -0400

    update blog.jsx

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -7,7 +7,19 @@ const Blog = () => {
             <div className="container text-center">
                 <h2 className="heading">A Blog about Software Development and Life</h2>
                 <div className="intro">Welcome to my blog. Subsribe and get my latest blog post in your inbox</div>
-                
+                <div className="single-form-max-width pt-3 mx-auto">
+                    <form action="" className="signup-form row g-2 g-lg-2 align-items-center"></form>
+                    <div className="col-12 col-md-9">
+                        <label for="semail" className="sr-only">Your email</label>
+                        <input 
+                            type="email" 
+                            id="semail" 
+                            name="semail1" 
+                            className="form-control me-md-1 semail" 
+                            placeholder='Enter email' 
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     )
